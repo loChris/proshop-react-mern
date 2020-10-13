bfaf3f36266efe11720dc4ebafda5f0e4907bba6
loChris <44450518+loChris@users.noreply.github.com>
2020-10-13 14:58:36 -0400

    Added .env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 const app = express();
-const PORT = 5000;
 
 app.get('/', (req, res) => {
 	res.send('API is running');
@@ -16,6 +18,9 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product);
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	);
 });
