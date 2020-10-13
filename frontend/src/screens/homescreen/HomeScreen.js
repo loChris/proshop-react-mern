4d988de5e8dbb9c6f098a3442ece5e92b55ecb9a
loChris <44450518+loChris@users.noreply.github.com>
2020-10-13 12:16:16 -0400

    Added axios and connected the frontend with the backend. Products on the homescreen and product page are now fetched from the backend api instead of a js file on the frontend.

--- frontend/src/screens/homescreen/HomeScreen.js
+++ frontend/src/screens/homescreen/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Product from '../../components/product/Product';
 import { Row, Col } from 'react-bootstrap';
-import products from '../../products';
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('/api/products'); //Added proxy in package.json Destructure for ease of use and get products from backend
+			setProducts(data);
+		};
+		fetchProducts();
+	}, []);
+
 	return (
 		<>
 			<h1>Latest Products</h1>
