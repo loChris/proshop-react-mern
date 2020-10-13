39e2ee0f2b5525296adb69e72e267eedcd68013e
loChris <44450518+loChris@users.noreply.github.com>
2020-10-13 11:03:21 -0400

    Implemented React Router and set up the component paths.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import HomeScreen from './screens/homescreen/HomeScreen';
+import ProductScreen from './screens/productscreen/ProductScreen';
 import './App.css';
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<Header />
 			<main className="py-3">
 				<Container>
-					<HomeScreen />
+					<Route exact path="/" component={HomeScreen} />
+					<Route path="/product/:id" component={ProductScreen} />
 				</Container>
 			</main>
 			<Footer />
-		</>
+		</Router>
 	);
 };
 
